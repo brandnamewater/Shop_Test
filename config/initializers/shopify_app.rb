32575ec4c467f5c1270fac9d78724463cacbbc73
brandnamewater <brandnamewater@gmail.com>
2018-10-13 07:04:14 -0400

    testing

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -6,4 +6,13 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
+
+  config.webhooks = [
+      {topic: 'app/uninstalled', address: "#{ENV['WEBHOOK_URL']}app_uninstalled", format: 'json'},
+      {topic: 'orders/delete', address: "#{ENV['WEBHOOK_URL']}orders_delete", format: 'json'},
+      {topic: 'orders/create', address: "#{ENV['WEBHOOK_URL']}orders_create", format: 'json'},
+      {topic: 'orders/updated', address: "#{ENV['WEBHOOK_URL']}orders_updated", format: 'json'}
+    ]
+
+
 end
